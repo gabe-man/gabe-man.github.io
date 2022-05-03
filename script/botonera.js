6bc1feaca602d9c8788269907bd004ca24d41ce4
gabe-man <92256369+gabe-man@users.noreply.github.com>
2022-05-03 14:34:14 +0200

    Add files via upload

---
+++ script/botonera.js
@@ -0,0 +1,11 @@
+function resaltar(event) {
+	event.style.color = "#0000ff";
+}
+
+function normal(event) {
+	event.style.color = "#ffffff";
+}
+
+function clickea(event) {
+	event.style.color = "#ff00ff";
+}
